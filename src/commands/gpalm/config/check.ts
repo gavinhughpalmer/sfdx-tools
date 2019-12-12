6eda83884e1ca0dbc360fef784fdb0a60efd4c62
Gavin Palmer <gavinhughpalmer@gmail.com>
2019-12-12 21:36:20 +0000

    Adding in additional config checks to cover validation rules and enforce no '= true' or '=false' in formula fields

--- src/commands/gpalm/config/check.ts
+++ src/commands/gpalm/config/check.ts
@@ -1,4 +1,4 @@
-import { core, SfdxCommand } from '@salesforce/command';
+import { core, flags, SfdxCommand } from '@salesforce/command';
 import * as fs from 'fs';
 import * as glob from 'fast-glob';
 
@@ -22,14 +22,18 @@ export default class Check extends SfdxCommand {
   `
     ];
 
-    protected static flagsConfig = {};
+    protected static flagsConfig = {
+        // TODO add flag for adding additional types to ignore
+        targetdir: flags.string({ char: 'd', description: 'The directory that should be targeted for the check of configuration', default: 'force-app' })
+    };
     protected static requiresUsername = false;
     protected static supportsDevhubUsername = false;
     protected static requiresProject = true;
 
     public async run(): Promise<Array<FileError>> {
         const fileErrors : Array<FileError> = [];
-        const flowFilePaths = await glob(['force-app/**/*.flow-meta.xml']);
+        const targetDir = this.flags.targetdir;
+        const flowFilePaths = await glob([targetDir + '/**/*.flow-meta.xml']);
 
         const processBuilderObjects = new Set();
         for (let flowFilePath of flowFilePaths) {
@@ -51,28 +55,57 @@ export default class Check extends SfdxCommand {
             if (isProcessBuilder && !flowContents.includes(skipProcess)) {
                 fileErrors.push({filePath: flowFilePath, errorMessage: 'The process builder does not include the line ' + skipProcess});
             }
+            if (flowContents.match(/<formulas>[^]+<expression>[^]+=\s*(false|true)[^]*<\/expression>[^]+<\/formulas>/i)) {
+                fileErrors.push({filePath: flowFilePath, errorMessage: 'The process builder formula contains a comparison of a checkbox (boolean) to the keyword true or false, this is unnessisary as the boolean itself can be used'});
+            }
         }
 
         const metadataFilesWithDescriptions = await glob([
-            'force-app/**/*__c.object-meta.xml',
-            'force-app/**/*__c.field-meta.xml',
-            'force-app/**/*.permissionset-meta.xml',
-            'force-app/**/*.validationRule-meta.xml',
-            'force-app/**/*.duplicateRule-meta.xml',
-            'force-app/**/*.quickAction-meta.xml'
+            targetDir + '/**/*__c.object-meta.xml',
+            targetDir + '/**/*__c.field-meta.xml',
+            targetDir + '/**/*.permissionset-meta.xml',
+            targetDir + '/**/*.validationRule-meta.xml',
+            targetDir + '/**/*.duplicateRule-meta.xml',
+            targetDir + '/**/*.quickAction-meta.xml'
         ]);
-        console.log(metadataFilesWithDescriptions);
         for (let metaFilePath of metadataFilesWithDescriptions) {
-            if (metaFilePath.match(/.*__[a-zA-Z0-9]*__c.*/)) {
-                console.log(metaFilePath);
-            }
+            const isNotManagedMetadata = !metaFilePath.match(/[\w\d]+__[\w\d]+__c.\w+-meta\.xml/);
             const metaFileContents = fs.readFileSync(metaFilePath, 'utf8');
-            if (!metaFileContents.match(/<description>.*<\/description>/)) {
+            if (isNotManagedMetadata && !metaFileContents.match(/<description>[^]*<\/description>/)) {
                 fileErrors.push({filePath: metaFilePath, errorMessage: 'The file does not include a description'});
             }
         }
+
+        const metaFilesWithFormulas = await glob([
+            targetDir + '/**/*__c.field-meta.xml',
+            targetDir + '/**/*.flow-meta.xml',
+            targetDir + '/**/*.workflow-meta.xml',
+            targetDir + '/**/*.approvalProcess-meta.xml'
+        ]);
+        // <formulas><expression>
+        for (let metaFilePath of metaFilesWithFormulas) {
+            const isNotManagedMetadata = !metaFilePath.match(/[\w\d]+__[\w\d]+__c.\w+-meta\.xml/);
+            const metaFileContents = fs.readFileSync(metaFilePath, 'utf8');
+            if (isNotManagedMetadata && metaFileContents.match(/<formula>[^]+=\s*(false|true)[^]*<\/formula>/i)) {
+                fileErrors.push({filePath: metaFilePath, errorMessage: 'The formula contains a comparison of a checkbox (boolean) to the keyword true or false, this is unnessisary as the boolean itself can be used'});
+            }
+        }
+
+        const validationMetaFiles = await glob([targetDir + '/**/*.validationRule-meta.xml']);
+        for (let metaFilePath of validationMetaFiles) {
+            const isNotManagedMetadata = !metaFilePath.match(/[\w\d]+__[\w\d]+__c.\w+-meta\.xml/);
+            const metaFileContents = fs.readFileSync(metaFilePath, 'utf8');
+            if (isNotManagedMetadata && metaFileContents.match(/<errorConditionFormula>[^]+=\s*(false|true)[^]*<\/errorConditionFormula>/i)) {
+                fileErrors.push({filePath: metaFilePath, errorMessage: 'The validation formula contains a comparison of a checkbox (boolean) to the keyword true or false, this is unnessisary as the boolean itself can be used'});
+            }
+
+            const skipValidations = 'NOT($Setup.Configuration__c.Are_Validations_Off__c)';
+            if (!metaFileContents.includes(skipValidations)) {
+                fileErrors.push({filePath: metaFilePath, errorMessage: 'The validation rule does not include the line ' + skipValidations});
+            }
+        }
         // TODO what should be returned...
-        // console.log(fileErrors);
+        console.log(fileErrors);
         return fileErrors;
     }
 }
